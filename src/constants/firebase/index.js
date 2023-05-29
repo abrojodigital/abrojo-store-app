9dec9cc5a8db102a75b4675ee14ed1598e546c0d
abrojodigital <abrojodigital@gmail.com>
2023-05-29 18:35:46 -0300

    Variables de entorno

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,2 @@
-export const FIREBASE_REALTIME_DB_URL = "https://abrojo-store-default-rtdb.firebaseio.com/";
+// export const FIREBASE_REALTIME_DB_URL = "https://abrojo-store-default-rtdb.firebaseio.com/";
+export const FIREBASE_REALTIME_DB_URL = process.env.FIREBASE_REALTIME_DB_URL;
