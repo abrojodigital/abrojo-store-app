ca8b69461f3240dc7c9c41edd839632695637e45
abrojodigital <abrojodigital@gmail.com>
2023-05-29 19:43:29 -0300

    Firebase URL

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,2 +1,1 @@
-// export const FIREBASE_REALTIME_DB_URL = "https://abrojo-store-default-rtdb.firebaseio.com/";
-export const FIREBASE_REALTIME_DB_URL = process.env.FIREBASE_REALTIME_DB_URL;
+export const FIREBASE_REALTIME_DB_URL = "https://abrojo-store-default-rtdb.firebaseio.com/";
