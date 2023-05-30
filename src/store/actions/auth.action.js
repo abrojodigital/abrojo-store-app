83390ff8729f55bb3f34b3ad437b8e1bc70abcbf
abrojodigital <abrojodigital@gmail.com>
2023-05-30 19:43:21 -0300

    Implemento signIn

--- src/store/actions/auth.action.js
+++ src/store/actions/auth.action.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/order */
 import { FIREBASE_AUTH_SIGN_IN_URL, FIREBASE_AUTH_SIGN_UP_URL } from "../../constants";
+
 import { authTypes } from "../types";
 
 const { SIGN_IN, SIGN_UP } = authTypes;
@@ -36,5 +38,33 @@ export const signUp = ({ email, password }) => {
 };
 
 export const signIn = ({ email, password }) => {
-  console.log("signIn");
+  return async (dispatch) => {
+    try {
+      const response = await fetch(FIREBASE_AUTH_SIGN_IN_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      dispatch({
+        type: SIGN_IN,
+        token: data.idToken,
+        userId: data.localId,
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
 };
